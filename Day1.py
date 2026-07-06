65fab0978357f19bff89e3ced2df36e9278cafcc
mukundk1508-06072026 <mukundk.work24999@gmail.com>
2026-07-06 12:06:34 +0530

    Initial Changes

---
+++ Day1.py
@@ -0,0 +1,7 @@
+## checking if the environment is working / not.
+'''print("Hello World")
+print("This will be marking all the practice scripts and learnings related to python");
+resources = "https://www.udemy.com/share/103J8C3@5X4F98BK-9gGlp3buD_tyLnpMinHJvQmUdsLddBftMp9ESSx2lcl2JJoqULUzSZl/"
+print(resources);'''
+
+#############################################################
