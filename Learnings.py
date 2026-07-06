464a87cc6621106ec985142782940fce4c48ca8a
mukundk1508-06072026 <mukundk.work24999@gmail.com>
2026-07-06 12:17:14 +0530

    changing the name from Day1.py to Learnings.py

--- Learnings.py
+++ Learnings.py
@@ -3,5 +3,5 @@
 print("This will be marking all the practice scripts and learnings related to python");
 resources = "https://www.udemy.com/share/103J8C3@5X4F98BK-9gGlp3buD_tyLnpMinHJvQmUdsLddBftMp9ESSx2lcl2JJoqULUzSZl/"
 print(resources);'''
-
 #############################################################
+## Day 1 :- Printing, Commenting, Debugging, String Manupiulation and Variable Names.
